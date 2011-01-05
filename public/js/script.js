c8e3e46a2a989b34ea4b561d254d3a11fd0bb6fd
shyndman <scotty.hyndman@gmail.com>
2011-01-04 19:45:16 -0500

    auto find location in

--- public/js/script.js
+++ public/js/script.js
@@ -68,7 +68,7 @@ ttc = {
 	
 	/** Sets the map location and zoom, and looks for streetcars */
 	onFindLocation: function(position) {
-		ttc.getAndDisplayStops(position.coords);
+		ttc.getAndDisplayStops({lat: position.coords.latitude, lng: position.coords.longitude});
 /*		console.log(position);
 		var g = google.maps;
 		ttc.map.setCenter(
